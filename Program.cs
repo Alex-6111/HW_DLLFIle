b85736d420c4c8bc2d974d6a6a33b5b827574d8d
Admin <obekas777@gmail.com>
2022-09-07 14:06:54 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿
+using System;
+using System.Drawing;
+
+namespace HW_DLLFIle
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+            GetMonitorDiagonal();
+
+        }
+
+        public static double GetMonitorDiagonal()
+        {
+            string displayWidth = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height.ToString();
+            string displayHeight = System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width.ToString();
+            return Math.Sqrt(Math.Pow(Convert.ToDouble(displayWidth), 2) + Math.Pow(Convert.ToDouble(displayHeight), 2)) / 96;
+        }
+    }
+}
